3c90cb290727e43fd5a5c2bd6ff74c4bf80ba3d5
Jordan Louie <Jordan Louie>
2016-01-11 11:47:13 -0500

    Playa class added con inst vars

---
+++ Playa.java
@@ -0,0 +1,10 @@
+public class Playa extends Character {
+
+    private String name;
+    private String appearence;
+    private String dialogue;
+    private int emotion;
+
+    private int stamina;
+    private int days = 30;
+}
