cfed6a9697b2b722cf8acc70107c9bd95b7857dd
Jordan Louie <Jordan Louie>
2016-01-12 11:58:51 -0500

    updated Setting.java with list of places

--- Setting.java
+++ Setting.java
@@ -1,5 +1,28 @@
 public class Setting {
 
+    /*
+      map
+
+      places to meet people
+        sansland
+	hospital/doctors office
+	
+	mall
+	  mcdonuds -- meet cashier or donut
+	  forever54
+	  bed, toilet and nowhere (sofa, sink, and somewhere)
+	  something like gamestop
+	school
+	  class
+	  cafeteria
+	  auditorium
+
+      date locations
+        resturant 
+	beach
+	
+      
+     */
     protected String image;
     protected String text;
 
