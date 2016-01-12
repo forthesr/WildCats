def5dd391b4933efc6f980c6db3c08a2ebc2ae64
UR <lshi@stuy.edu>
2016-01-11 21:15:20 -0500

    Update Playa.java
    Added basic constructor goals

--- Playa.java
+++ Playa.java
@@ -7,4 +7,14 @@ public class Playa extends Character {
 
     private int stamina;
     private int days = 30;
+    
+    
+    //Constructors
+    /*    setName - replace default name "" to user input
+    setAppearance - set as ""
+    setEmotion - default 1, happy
+    setStamina - set to 12
+    setDialogue - set as ""
+    */
+    
 }
