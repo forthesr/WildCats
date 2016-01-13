4325dfed71978b5ff6241dd8943d3562e5fba2f7
UR <lshi@stuy.edu>
2016-01-12 23:14:56 -0500

    Update Playa.java

--- Playa.java
+++ Playa.java
@@ -1,12 +1,12 @@
 public class Playa extends Character {
 
-    private String name;
-    private String appearence;
-    private String dialogue;
-    private int emotion;
+    protected String name;
+    protected String appearence;
+    protected String dialogue;
+    protected int emotion;
 
-    private int stamina;
-    private int days = 30;
+    public int stamina;
+    public int days = 30;
     
     
     //Constructors
