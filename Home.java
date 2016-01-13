24458d22a85e07a5f1fcaaeee7e96e8e151e35f4
UR <lshi@stuy.edu>
2016-01-12 23:05:01 -0500

    Update Home.java
    Added sleep method

--- Home.java
+++ Home.java
@@ -1,6 +1,35 @@
+import java.util.Scanner;
+
 public class Home extends Setting {
 
-  protected String image;
+  protected String[] image;
   protected String text;
   
+  //constructors
+  public Home(){
+    image = "";
+    text = "Welcome home! WOuld you like to rest?
+            Resting will regain full stamina and one day will pass."
+  }
+  
+  //methods
+  //gets
+  public String[] getImage(){
+    return image;
+  }
+  
+  public String getText(){
+    return text;
+  }
+  
+  //sleep
+  public void sleep(){
+    System.out.println("Good night!");
+    stamina = 15;
+    days --;
+    System.out.println(image);
+    System.out.println(text);
+  }
+  
+
 }
