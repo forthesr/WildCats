bf6807d28382f2d48aceef2afd4b08da71245397
UR <lshi@stuy.edu>
2016-01-14 21:48:32 -0500

    Update DatingSim.java
    Added view, overlay, datingPartner, etc.

--- DatingSim.java
+++ DatingSim.java
@@ -5,9 +5,54 @@
 
 //Driver File
 
+
 public class DatingSim {
     
-    protected String datingPartner;
+    protected DatableSO datingPartner;
     protected String[][] view;
+    private final int VIEW_LENGTH = 3; 
+
+    public DatingSim(){
+      datingPartner = "";
+      //Make a view by populating it with blank spaces.
+      for(int i = 0; i < VIEW_LENGTH; i++)
+      {
+        for (int j = 0; j < VIEW_LENGTH; j++)
+        {
+          view[i][j] = " ";
+        }
+      }
+    }
+
+    public String[][] getView(){
+      return view;
+    }
+
+    public void setView(String[][] bg){
+      view = bg;
+    }
+
+    public DatableSO getDatingPartner(){
+      return datingPartner;
+    }
+
+    public void setDatingPartner(DatableSO person){
+      datingPartner = person;
+    }
+
+    public void overlay(DatableSO person){
+
+      //Person = new PErson(spersonNAme)
+      for (int x = view.length- person.getAppearance().length; x < view.length; x++){
+        for (int i = 0; i < person.getAppearance().length; i++){
+        view[x][2] = person[i];
+      } }
+    }
+
+    public static void main(String[] args){
+
+     // System.out.println(view.overlay(person) );
+
+    }
     
 }
