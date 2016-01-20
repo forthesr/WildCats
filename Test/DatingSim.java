fbb362cc70b4986e853bef83f543a50f78586188
Jordan Louie <Jordan Louie>
2016-01-20 11:35:51 -0500

    RW Intro name fix, datingSim edit

--- Test/DatingSim.java
+++ Test/DatingSim.java
@@ -103,10 +103,10 @@ public class DatingSim {
     }
 
     public void oneDay(){
-    	while (stamina > 0) {
-	setView(Map);//maybe make transport work for this
-	Map.transport();
-	currentPlace.transport();
+	while (stamina > 0) {
+	    setView(Map);//maybe make transport work for this
+	    Map.transport();
+	    currentPlace.transport();
 	
     	}
     	currentPlace.transportToHome(); //need to maybe make seperate method for this?
