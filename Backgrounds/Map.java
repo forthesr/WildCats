aab52c23223457fd2da3a212129df894f408844b
UR <lshi@stuy.edu>
2016-01-22 23:44:02 -0500

    Update Map.java

--- Backgrounds/Map.java
+++ Backgrounds/Map.java
@@ -7,10 +7,32 @@ public class Map extends Setting {
   
   //Constructor
   public Map(){
-  image = "";
+     private String[][] image = new String[][]
+	    {{"____________________","______________________________","____________________"}, //0
+	     {"       _____        ","                              ","        __________  "}, //1
+	     {"      / _ _ \_      ","   \\\    ///        ))       ","        ||||||||||  "},
+	     {"     / |_|_| \|     ","   ((O)  (O))   /)  (o0)-.    ","        ||-||||-||  "}, //3
+	     {"    /_________\     ","    | \  / |  (o)(O) | (_))   ","        ||||||||||  "},
+	     {"     |       |      ","    ||\\//||   //\\  | .-'    ","        ||| |.||||  "}, //5
+	     {"     |   _   |      ","    || \/ ||  |(__)| |(       ","  __   __    _  _   "},
+	     {"     |__|_|__|      ","    ||    ||  /,-. |  \)      "," / _| /_|| |/ \/ \| "}, //7
+	     {"                    ","   (_/    \_)-'   ''  (       "," \_ \||_ |-| .| .|| "},
+	     {"       _  _  _  _   ","      .....                   "," |__/ \_|| |\_/\_/| "}, //9
+	     {"   |_|/.\| \/ |/_\_ ","    ::::::::                  ","                    "},
+	     {"   | |\_/|_||_|\__/ ","    :::::::: .:.              ","                    "}, //11
+	     {"                    ","  .` ::::::  / :`       +     ","       +            "},
+	     {"                    "," .:\. \||//--      +  ________","__+_______          "}, //13
+	     {"                    ","  ` \__||/   \:.    +| _  _  _","_         |         "},
+	     {"       __  _  __    ","       ||    ::`     || \/ |/ ","_\ |  |   |  +      "}, //15
+	     {"      | _|/ || _||/ ","       ||          + ||_||_|_|"," | |_ |_  |         "},
+	     {"      || / -|| \ |\ ","       ||            |    _ _ ","          | +       "}, //17
+	     {"      ||/_/_||_`\| \","      /MM\           |___|_|_|","__________|         "},
+	     {"                    ","                              ","                    "}  //19
+	    };;
+
   text = "Where would you like to go?
           You can go to the following places:\n" +
-      "1-School\t2-Mall\3-Park\n" + 
+      "1-School\2-Mall\3-Park\4-Home\n" + 
       "Enter the name of the place you wish to go:";
   }
   
@@ -23,24 +45,11 @@ public class Map extends Setting {
     return text;}
     
     public void transport(){
-	String location;
-	//outputs question
-        System.out.println(text);
+        System.out.println(getText() );
         Setting choice = Keyboard.readString();
-        setView(choice.getImage() );
+        setView(choice.getImage() ); }
 
-	/*if (choice = 1) {
-	    DatingSim.view = School.getImage();
-	    location = "The School";
-	}
-	if (choice = 2) {
-	    DatingSim.view = Mall.getImage();
-	    location = "The Mall";
-	}
-	if (choice = 3) {
-	    DatingSim.view = Hospital.getImage();
-	    location = "The Hospital";
-	    }*/
+
 
 	DatingSim.view = choice.getImage();
 	System.out.println (choice);
