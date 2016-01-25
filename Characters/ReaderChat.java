ebcb1bee35f45b61c7afa8286a917715f9bf7d32
Jordan Louie <Jordan Louie>
2016-01-24 21:29:37 -0500

    ReaderChat update to work for final product

--- Characters/ReaderChat.java
+++ Characters/ReaderChat.java
@@ -1,15 +1,17 @@
 import java.io.*; //This is to use Java's FileReader class.
 import java.util.ArrayList;
 
+import cs1.Keyboard;
+
 import java.util.Scanner; //This is to use Java's Scanner class.
 //file reader = scanner!  They're basicaly the same thing.
 
 
 public class ReaderChat {
-    public static void main(String [] args) {
 
-        // The name of the file to open.
-        String fileName = "RichardChat1.txt";  //is in same folder
+    public static void readerChat(DateableSO o, String fileName) {
+	// The name of the file to open.
+        //String fileName = "RichardChat1.txt";  //is in same folder
        
         try{
 	    //Make a reader thing
@@ -19,13 +21,13 @@ public class ReaderChat {
 	    reader.useDelimiter("~");
 
 	    String section1, section2, section3, section4, section5;
-	      section1 = "";
-	      section2= "";
-	      section3 = "";
-	      section4= "";
-	      section5 = "";
+	    section1 = "";
+	    section2= "";
+	    section3 = "";
+	    section4= "";
+	    section5 = "";
 
-	      while(reader.hasNext())
+	    while(reader.hasNext())
 		{
 		    section1 = reader.next();
 		    section2 = reader.next();
@@ -33,6 +35,8 @@ public class ReaderChat {
 		    section4 = reader.next();
 		    section5 = reader.next();
 		}
+
+	    String stringSeperator = "\\r?\\n";
 	    
 	    //ArrayList to hold lines
 	    ArrayList<String> sectionsList = new ArrayList<String>();
@@ -50,50 +54,47 @@ public class ReaderChat {
 	      3-4: chosen dialogue
 	    */
 
-	    String stringSeperator = "\\r?\\n";
 	    ArrayList<String> chatOptions = new ArrayList<String>();
-	    chatOptions = section4.split(stringSeperator);
+	    for (String s : section4.split(stringSeperator)){
+		chatOptions.add(s);
+	    }
 	    
 	    ArrayList<String> chatResponses = new ArrayList<String>();
-	    chatResponses = section5.split(stringSeperator);
+	    for (String s : section5.split(stringSeperator)){
+		chatResponses.add(s);
+	    }
 
-	    /*for(int i = 0; i < sectionsList.size()/4; i++)
-		{
-		    System.out.println("Name:\t" + sectionsList.get(4*i));
-		    System.out.println("Mood:\t" + sectionsList.get(4*i + 1));
-		    System.out.println("Line:\t" + sectionsList.get(4*i + 2));
-		    System.out.println("\n");
-		    }*/
-	    for(int i = 0; i < sectionsList.size()/3; i++) {
-		if (sectionsList.get(3*i) == name) {
-		    System.out.println("NAME: " + sectionsList.get(3*i + 2));
+	    for(int i = 0; i < sectionsList.size()/4; i++) {
+		if (sectionsList.get(4*i).equals( "name")) {
+		    System.out.println(playa.name + ": " + sectionsList.get(4*i + 2));
 		    System.out.println("\n");
 		}
 		else {
-		    datingPartner.setEmotion(parseInt(sectionsList.get(3*i + 1)));
-		    datingPartner.setAppearance();
-		    System.out.println(datingPartner.getAppearance());
-		    System.out.print(sectionsList.get(3*i) + ": ");
-		    System.out.println(sectionsList.get(3*i + 2));
+		    String x = (sectionsList.get(4*i + 1)).substring(0,1);
+		    o.setEmotion(Integer.parseInt(x));
+		    o.setAppearance();
+		    System.out.println(o.stringAppearance());
+		    System.out.print(sectionsList.get(4*i) + ": ");
+		    System.out.println(sectionsList.get(4*i + 2));
 		    System.out.println("\n");
 		}
 	    }
 
 	    System.out.println("How do you want to respond?: (type the number)");
 	    for (int i = 0; i < chatOptions.size()/4; i++) {
-		System.out.println(i + ": " + chatOptions.get(4*i + 2));
+		System.out.println(i + ": " + chatOptions.get(4*i + 3));
 	    }
 
 	    int input = Keyboard.readInt();
 
-	    System.out.println("NAME: " + chatOptions.get(4*input + 2) + "\n");
+	    System.out.println(playa.name + ": " + chatOptions.get(4*input + 3) + "\n");
 
 	    //Set and print DateableSO picture and chat
-	    datingPartner.setEmotion(parseInt(sectionsList.get(3*input + 1)));
-	    datingPartner.setAppearance();
-	    System.out.println(datingPartner.getAppearance());
-	    System.out.print(sectionsList.get(3*input) + ": ");
-	    System.out.println(sectionsList.get(3*input + 2));
+	    o.setEmotion(Integer.parseInt(chatResponses.get(4*input + 2)));
+	    o.setAppearance();
+	    System.out.println(o.stringAppearance());
+	    System.out.print(chatResponses.get(4*input + 1) + ": ");
+	    System.out.println(chatResponses.get(4*input + 3));
 	    System.out.println("\n");
 	    
 	    //very important! close things!  when you are done!
@@ -102,5 +103,11 @@ public class ReaderChat {
 	catch(FileNotFoundException ex){
 	    System.out.println("Had a problem finding the file");
         }
+	
+    }
+    
+    public static void main(String [] args) {
+	Richard richard = new Richard();
+	readerChat(richard, "RichardChat1.txt");
     }
 }
